0a405d0a8f05a82f8cb5a6429b02a3a7f09c3348
Alexander Mironov <goupriver@gmail.com>
2022-08-30 20:35:59 +0700

    feat: дабавил асинхронный запрос на сервер

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  { id: '1', name: "Mac Baren" },
-  { id: '2', name: "Philip Morris" },
-  { id: '3', name: "James Bond" },
-];
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await fetch("http://localhost:3003/users");
+  const data = await response.json();
+
+  return data;
+});
+
+const initialState = [];
 
 export const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // Immer позволяет нам обновлять состояние двумя способами: либо изменяя
+      // существующее значение состояния, либо возвращая новый результат.
+      return action.payload;
+    });
+  },
 });
 
 export default usersSlice.reducer;
